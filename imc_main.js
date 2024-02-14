ebc6a3313264c0d16f91c22617bfe71c6ed0d64d
João Victor <joao_nicolai@estudante.sesisenai.org.br>
2024-02-14 08:49:28 -0300

    Criando o front end

--- imc_main.js
+++ imc_main.js
@@ -1,2 +1,11 @@
 let resposta = document.getElementById('resposta')
 let calcular = document.getElementById('calcular')
+
+calcular.addEventListener('click', ()=>{
+    let nome = document.getElementById('nome').value
+    let idade = Number(document.getElementById('idade').value)
+    let altura = Number(document.getElementById('altura').value)
+    let peso = Number(document.getElementById('peso').value)
+    
+    console.log(nome, idade, peso, altura)
+})
